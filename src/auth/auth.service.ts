02a3519b319f8e808584670a1bf43ccfa0b4702d
Andrew Weinstein <andrew@altvia.com>
2020-11-13 22:58:36 -0700

    respond to login with JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDTO } from './dtos/auth-credentials.dto';
-import { User } from './user.entity';
+import { JwtPayload } from './jwt-payload.interface';
+import { JwtResponse } from './jwt-response.interface';
 import { UserRepository } from './user.repository';
 
 @Injectable()
@@ -9,19 +11,22 @@ export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private readonly userRepository: UserRepository,
+    private readonly jwtService: JwtService,
   ) {}
 
   async signUp(userDTO: AuthCredentialsDTO): Promise<void> {
     return await this.userRepository.signUp(userDTO);
   }
 
-  async signIn(userDTO: AuthCredentialsDTO): Promise<string> {
-    const user = await this.userRepository.signIn(userDTO);
+  async signIn(userDTO: AuthCredentialsDTO): Promise<JwtResponse> {
+    const username = await this.userRepository.signIn(userDTO);
 
-    if (!user) {
+    if (!username) {
       throw new UnauthorizedException('Invalid credentials.');
     }
 
-    return user;
+    const payload: JwtPayload = { username };
+    const accessToken = this.jwtService.sign(payload);
+    return { accessToken };
   }
 }
