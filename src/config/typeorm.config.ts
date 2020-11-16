7d5d493ff1fb4a5dec5206013ea506d3d0ae2fce
Andrew Weinstein <andrew@altvia.com>
2020-11-15 17:50:47 -0700

    more production setup

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,16 +1,22 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DbConfig } from 'src/config.interface';
 import * as config from 'config';
+import { Logger } from '@nestjs/common';
 
 const dbConfig: DbConfig = config.get('db');
 
+const logger = new Logger('TypeORMConfig');
+logger.log(`db host: ${process.env.RDS_HOSTNAME}`);
+logger.log(`db user: ${process.env.RDS_USERNAME}`);
+logger.log(`db name: ${process.env.RDS_DB_NAME}`);
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
-  host: process.env.DB_HOST || dbConfig.host,
-  port: parseInt(process.env.DB_PORT) || dbConfig.port,
-  username: process.env.DB_USERNAME || dbConfig.username,
-  database: process.env.DB_NAME || dbConfig.database,
-  password: process.env.DB_PASSWORD || dbConfig.password,
+  host: process.env.RDS_HOSTNAME || dbConfig.host,
+  port: parseInt(process.env.RDS_PORT) || dbConfig.port,
+  username: process.env.RDS_USERNAME || dbConfig.username,
+  database: process.env.RDS_DB_NAME || dbConfig.database,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
   entities: [__dirname + '/../**/*.entity.{ts,js}'],
   synchronize: process.env.DB_SYNC === 'true' || dbConfig.synchronize,
 };
